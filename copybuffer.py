278abf60fcd6acd53d028e07aceceb8996b7fc76
Arif <arifkhan65@gmail.com>
2020-06-01 13:09:54 +0200

    Initial commit

---
+++ copybuffer.py
@@ -0,0 +1,10 @@
+
+list=["apple","boroi","jamboora","boroi","kola","angoor","mishti","boroi","kola"]
+x=input('type the name of your fruit: ')
+r=(list.count(x))
+print(r)
+x1=(list.pop(-2))
+print('the second last element in friut list is',x1)
+x2=list.copy()
+
+print(x2)
